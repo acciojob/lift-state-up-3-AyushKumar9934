3c38ad4316c773f7d44469bcd2742b47d5957dee
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-27 21:45:41 +0530

    third commit

--- src/components/ChildComponent1.js
+++ src/components/ChildComponent1.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ChildComponent1 = (props) => {
   return (
     <div style={{margin:'5px',backgroundColorolor:"#ef574c",width:"100%",height:"20px"}}>Child Component 1
-    <button style={{color:"white"}} onClick={()=>props.setSelectedOption("Option1")} >Option1 </button>
+    <button style={{color:"white"}} onClick={()=>props.setSelectedOption("Option 1")} >Option 1 </button>
     
     </div>
   )
--- src/components/ChildComponent2.js
+++ src/components/ChildComponent2.js
@@ -4,7 +4,7 @@ const ChildComponent2 = (props) => {
   return (
     
     <div style={{margin:'5px',color:"#ef574c",width:"100%",height:"20px"}}>Child Component 2
-    <button style={{color:"white"}} onClick={()=>props.setSelectedOption("Option2")} >Option2 </button>
+    <button style={{color:"white"}} onClick={()=>props.setSelectedOption("Option 2")} >Option 2 </button>
     
     </div>
   )
